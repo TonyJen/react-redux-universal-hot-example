e86c0a7f0a3fadfd9bc6fc0d53d8c4301749ea4c
Tony Jen <user@users-MacBook-Pro.local>
2016-02-27 11:59:57 -0500

    Add state change to experiment module.

--- src/containers/Experiment/Experiment.js
+++ src/containers/Experiment/Experiment.js
@@ -5,50 +5,25 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 @connect(
-  (state) => ({liked: true}),
+  (state) => ({liked: state.experiment.liked}),
   dispatch => bindActionCreators({toggle}, dispatch)
 )
 export default class Experiment extends Component {
   static propTypes = {
-    liked: PropTypes.object,
+    liked: PropTypes.bool,
     toggle: PropTypes.func.isRequired
-  }
-  state = {liked: true,
-           list: this.createLongList()};
-
-  createLongList() {
-    const longNumber = 1000;
-    const foo = [];
-    let i = 0;
-    for (i = 0; i <= longNumber; i++) {
-      foo.push(i);
-    }
-    return foo;
   }
 
   render() {
-    const t1 = performance.now();
-    const text = this.state.liked ? 'like' : 'haven\'t liked';
-
-    const handleClick = ()=> {
-      toggle();
-    };
+    const { liked, toggle} = this.props; // eslint-disable-line no-shadow
+    const text = liked ? 'like' : 'haven\'t liked';
+    console.log('current liked state ' + liked);
     return (
       <div className="container">
       <Helmet title="Experient Page"/>
       <h1>Experiment Page</h1>
-      {
-        this.state.list.map((foo) => {
-          return <div>Hello, {foo}!</div>;
-        })
-      }
-      <p onClick={handleClick}>
+      <p onClick={toggle}>
         You {text} this. Click to toggle.
-      </p>
-      <p>
-        This render took about {
-             performance.now() - t1
-          } milliseconds.
       </p>
       </div>
     );
--- src/redux/modules/experiment.js
+++ src/redux/modules/experiment.js
@@ -7,7 +7,9 @@ const initialState = {
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case TOGGLE:
+      console.log('reducer liked state ' + state.liked);
       return {
+        ...state,
         liked: !state.liked
       };
     default:
